67691042796fd114bad7a1402abbe0fd1e6c73a0
Aivorlan <aivorlan@icloud.com>
2020-12-30 04:45:39 -0600

    Constructing actual deployable app with necessary functions and imports

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from './components/Navbar'
+import Header from './components/Header'
+import About from './components/About'
+import Portfolio from './components/Portfolio'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import portfolio from './portfolio.json'
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Header />
+        <About />
+        {portfolio.map( portfolios => <Portfolio key={portfolios.id} {...portfolios}/>)}
+        <Contact />
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
